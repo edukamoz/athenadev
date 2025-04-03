aff4590e93b5a48f289bc4924b001cf0e3d0449f
Eduardo Kamo <eduardokamoz@gmail.com>
2025-04-02 21:53:17 -0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,20 @@ import './Header.css'
 
 const Header = (props) => {
     return (
-       <header>oi</header>
+       <header>
+            <div className='logo'>
+                <img id='logo-site' src='images/logo.png' alt='logo Athena.dev'/>
+                <h1 id='titulo-logo'>Athena.dev</h1>
+            </div>
+            <nav className='menu'>
+                <ul className='lista-menu'>
+                    <li><a href='https://google.com' target='_blank'>Desafios</a></li>
+                    <li><a>Jogos</a></li>
+                    <li><a>Recursos</a></li>
+                    <li><a>Login</a></li>
+                </ul>
+            </nav>
+       </header>
     )
 }
 
